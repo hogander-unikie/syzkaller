e03ca3cb1c3f9a49424d9bbb5efc69794d790ab9
Jouni Hogander <jouni.hogander@unikie.com>
2020-08-28 08:35:02 +0300

    docs/config_bisect_ebaluation/run.sh: add flags to run w/ or w/o config bisect
    Signed-off-by: Jouni Hogander <jouni.hoegander@partner.bmw.de>

--- docs/config_bisect_evaluation/run.sh
+++ docs/config_bisect_evaluation/run.sh
@@ -13,7 +13,8 @@ SYZKALLER_BRANCH=flaky_flag
 SYZKALLER_REPROS_REPOSITORY=https://github.com/hogander-unikie/syzkaller-repros.git
 SYZKALLER_REPROS_BRANCH=bisect
 KERNEL_BRANCH=v4.19.59
-BISECT_ONLY=true
+WITHOUT_CONFIG_BISECT_ONLY=false
+WITH_CONFIG_BISECT_ONLY=true
 #REPRODUCER_LIST=76868b4c83962eefbf8015f01aeb9da4189fc25e 703f732a1f1f5e1d8314bb1a98eca20309420116 68fe3119847862315e52aa14961144b5a909bc23 9b519f4f0bcaeb000ba93389eda00310a6020abe be39d3abb5842e873d15d019b19f5ebef17c604a 55fc56e39caaf4f597fdbf388108892196d55f3f 68958b9d3651e09f0651b08f039f40c415fd02e6 ee7cf202a47281cda2e5a76bd1ba0683a10c2a65 4b61862ab93380cf84d66e09596ff3cbc3bc5341 ae0125a57674f57b675fad8f1440eb2be4790fba fb7ed6c3b2a69045e6b84a4ef30816f0f48791a9
 REPRODUCER_LIST="68958b9d3651e09f0651b08f039f40c415fd02e6 703f732a1f1f5e1d8314bb1a98eca20309420116 ce692a3aa13e00e335e090be7846c6eb60ddff7a 61318630f216fec89e9be95e621470084000d7bc 9b519f4f0bcaeb000ba93389eda00310a6020abe be39d3abb5842e873d15d019b19f5ebef17c604a 76868b4c83962eefbf8015f01aeb9da4189fc25e 55fc56e39caaf4f597fdbf388108892196d55f3f ee7cf202a47281cda2e5a76bd1ba0683a10c2a65 4b61862ab93380cf84d66e09596ff3cbc3bc5341 fb7ed6c3b2a69045e6b84a4ef30816f0f48791a9"
 
@@ -49,9 +50,12 @@ ln -s /usr/lib/x86_64-linux-gnu/libmpfr.so.6 hogander@hogander-HP-ZBook-15-G5:/u
 
 for reproducer in $REPRODUCER_LIST
 do
-    if [ $BISECT_ONLY != "true" ]
+    if [ $WITHOUT_CONFIG_BISECT_ONLY != "true" ]
     then
 	./syzkaller-repros/bisect.py --reproducer ./syzkaller-repros/linux/$reproducer.c  --kernel_repository  https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git --kernel_branch $KERNEL_BRANCH --chroot ./chroot  --baseline_config $BASELINE_CONFIG --reproducer_config $REPRODUCER_CONFIG --bisect_bin ./bisect_bin --syzkaller_repository $SYZKALLER_REPOSITORY --syzkaller_branch $SYZKALLER_BRANCH --output ./out_with_config_bisect ; mv ./out_with_config_bisect/syz-bisect.log ./out_with_config_bisect/$reproducer.log
     fi
-    ./syzkaller-repros/bisect.py --reproducer ./syzkaller-repros/linux/$reproducer.c  --kernel_repository  https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git --kernel_branch $KERNEL_BRANCH --chroot ./chroot --reproducer_config $REPRODUCER_CONFIG --bisect_bin ./bisect_bin --syzkaller_repository  $SYZKALLER_REPOSITORY --syzkaller_branch $SYZKALLER_BRANCH --output ./out_without_config_bisect ; mv ./out_without_config_bisect/syz-bisect.log ./out_without_config_bisect/$reproducer.log
+    if [ $WITH_CONFIG_BISECT_ONLY != true ]
+    then
+	./syzkaller-repros/bisect.py --reproducer ./syzkaller-repros/linux/$reproducer.c  --kernel_repository  https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git --kernel_branch $KERNEL_BRANCH --chroot ./chroot --reproducer_config $REPRODUCER_CONFIG --bisect_bin ./bisect_bin --syzkaller_repository  $SYZKALLER_REPOSITORY --syzkaller_branch $SYZKALLER_BRANCH --output ./out_without_config_bisect ; mv ./out_without_config_bisect/syz-bisect.log ./out_without_config_bisect/$reproducer.log
+    fi
 done
